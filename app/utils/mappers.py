8a82c6129533fe6a788348e52968132261a23f65
blakehatch <blake@tracemachina.com>
2025-02-03 15:03:22 -0500

    Add support for AVAX, Mode and Arb to scanners

--- app/utils/mappers.py
+++ app/utils/mappers.py
@@ -11,6 +11,9 @@ network_explorer_mapper = {
     NetworkEnum.ETH_SEPOLIA: "api-sepolia.etherscan.io",
     NetworkEnum.POLYGON: "api.polygonscan.com",
     NetworkEnum.POLYGON_AMOY: "api-amoy.polygonscan.com",
+    NetworkEnum.AVAX: "api.routescan.io",
+    NetworkEnum.MODE: "api.routescan.io",
+    NetworkEnum.ARB: "api.arbiscan.io",  # Added ARB
 }
 
 network_explorer_apikey_mapper = {
@@ -22,6 +25,9 @@ network_explorer_apikey_mapper = {
     NetworkEnum.ETH_SEPOLIA: os.getenv("ETHERSCAN_API_KEY"),
     NetworkEnum.POLYGON: os.getenv("POLYGONSCAN_API_KEY"),
     NetworkEnum.POLYGON_AMOY: os.getenv("POLYGONSCAN_API_KEY"),
+    NetworkEnum.AVAX: os.getenv("SNOWTRACE_API_KEY"),  # Added AVAX
+    NetworkEnum.MODE: os.getenv("MODESCAN_API_KEY"),  # Added MODE
+    NetworkEnum.ARB: os.getenv("ARBISCAN_API_KEY"),  # Added ARB
 }
 
 network_rpc_mapper = {
@@ -33,6 +39,9 @@ network_rpc_mapper = {
     NetworkEnum.ETH_SEPOLIA: "eth-sepolia.g.alchemy.com",
     NetworkEnum.POLYGON: "polygon-mainnet.g.alchemy.com",
     NetworkEnum.POLYGON_AMOY: "polygon-amoy.g.alchemy.com",
+    NetworkEnum.AVAX: "avax-mainnet.g.alchemy.com",  # Added AVAX
+    NetworkEnum.MODE: "mode-mainnet.g.alchemy.com",  # Added MODE
+    NetworkEnum.ARB: "arb-mainnet.g.alchemy.com",  # Added ARB
 }
 
 networks_by_type = {
@@ -41,11 +50,15 @@ networks_by_type = {
         NetworkEnum.BSC,
         NetworkEnum.ETH,
         NetworkEnum.POLYGON,
+        NetworkEnum.AVAX,  # Added AVAX
+        NetworkEnum.MODE,  # Added MODE
+        NetworkEnum.ARB,  # Added ARB
     ],
     NetworkTypeEnum.TESTNET: [
         NetworkEnum.BASE_SEPOLIA,
         NetworkEnum.BSC_TEST,
         NetworkEnum.ETH_SEPOLIA,
         NetworkEnum.POLYGON_AMOY,
+        # Add testnet equivalents for AVAX, MODE, and ARB if they exist
     ],
 }
