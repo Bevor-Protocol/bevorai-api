7f04fe1bb71923d023d1b489f56c6de208d4564b
Peter Simone <plssimone@gmail.com>
2025-02-14 19:09:32 -0500

    add txn to backfill

--- app/db/migrations/models/3_20250208170401_backfill_permissions.py
+++ app/db/migrations/models/3_20250208170401_backfill_permissions.py
@@ -1,25 +1,29 @@
 from tortoise import BaseDBAsyncClient
+from tortoise.transactions import in_transaction
 
 from app.db.models import App, Permission, User
 from app.utils.enums import AppTypeEnum, ClientTypeEnum
 
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
-    apps = await App.all().using_db(db)
-    users = await User.all().using_db(db)
+    async with in_transaction():
+        apps = await App.all().using_db(db)
+        users = await User.all().using_db(db)
 
-    permissions = []
-    for app in apps:
-        if app != AppTypeEnum.FIRST_PARTY:
+        permissions = []
+        for app in apps:
+            if app != AppTypeEnum.FIRST_PARTY:
+                permissions.append(
+                    Permission(client_type=ClientTypeEnum.APP, app_id=app.id)
+                )
+        for user in users:
             permissions.append(
-                Permission(client_type=ClientTypeEnum.APP, app_id=app.id)
+                Permission(client_type=ClientTypeEnum.USER, user_id=user.id)
             )
-    for user in users:
-        permissions.append(Permission(client_type=ClientTypeEnum.USER, user_id=user.id))
 
-    await Permission.bulk_create(objects=permissions, using_db=db)
+        await Permission.bulk_create(objects=permissions, using_db=db)
 
-    print(f"backfilled {len(permissions)} permission observations")
+        print(f"backfilled {len(permissions)} permission observations")
 
     # 🔹 Always return a valid SQL string (using a comment alone does not work)
     return "SELECT * FROM PERMISSION LIMIT 1;"
