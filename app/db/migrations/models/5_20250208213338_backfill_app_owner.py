e6095dc02d92c93538c2a181799f35832333489c
Peter Simone <plssimone@gmail.com>
2025-02-19 08:45:38 -0500

    use db in 005 migration

--- app/db/migrations/models/5_20250208213338_backfill_app_owner.py
+++ app/db/migrations/models/5_20250208213338_backfill_app_owner.py
@@ -7,16 +7,16 @@ from app.utils.enums import AuthScopeEnum
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     async with in_transaction():
-        auth = await Auth.first().select_related("app")
+        auth = await Auth.first(using_db=db).select_related("app")
         app_id = auth.app.id
         auth.scope = AuthScopeEnum.ADMIN
         await auth.save()
 
-        users = await User.all()
+        users = await User.all().using_db(db)
         for user in users:
             user.app_owner_id = app_id
 
-        await User.bulk_update(users, fields=["app_owner_id"])
+        await User.bulk_update(users, fields=["app_owner_id"], using_db=db)
 
         print(f"backfilled {len(users)} app_owner_id")
 
