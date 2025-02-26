e17d2a73f86929c677b4e79731e2936d5e778646
Peter Simone <plssimone@gmail.com>
2025-02-26 12:06:55 -0500

    add migration

--- app/db/models.py
+++ app/db/models.py
@@ -127,9 +127,7 @@ class Transaction(AbstractModel):
     user: fields.ForeignKeyNullableRelation[User] = fields.ForeignKeyField(
         "models.User", on_delete=fields.SET_NULL, null=True, related_name="transactions"
     )
-    type = fields.CharEnumField(
-        enum_type=TransactionTypeEnum, default=TransactionTypeEnum.SPEND
-    )
+    type = fields.CharEnumField(enum_type=TransactionTypeEnum)
     amount = fields.FloatField()
 
     class Meta:
